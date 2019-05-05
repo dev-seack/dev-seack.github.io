af67350328a7b831eb1f5f0811e295d164c3ed6f
dev-seack <dev@ts-dm.de>
2019-05-05 21:50:04 +0200

    added picture

--- scripts/legSlideshow.js
+++ scripts/legSlideshow.js
@@ -1,117 +1,166 @@
-if (typeof jQuery.when.all === 'undefined') {
-    jQuery.when.all = function (deferreds) {
-        return $.Deferred(function (def) {
-            $.when.apply(jQuery, deferreds).then(
-                function () {
-                    def.resolveWith(this, [Array.prototype.slice.call(arguments)]);
-                },
-                function () {
-                    def.rejectWith(this, [Array.prototype.slice.call(arguments)]);
-                });
-        });
-    }
+if (typeof jQuery.when.all === "undefined") {
+  jQuery.when.all = function(deferreds) {
+    return $.Deferred(function(def) {
+      $.when.apply(jQuery, deferreds).then(
+        function() {
+          def.resolveWith(this, [Array.prototype.slice.call(arguments)]);
+        },
+        function() {
+          def.rejectWith(this, [Array.prototype.slice.call(arguments)]);
+        }
+      );
+    });
+  };
 }
 
 (function($) {
-    function preloadImage(url)
-    {
-        var img=new Image();
-        img.src=url;
-    }
+  function preloadImage(url) {
+    var img = new Image();
+    img.src = url;
+  }
 
-    $.fn.legSlideshow = function( options ) {
-        var settings = $.extend({
-            slides: []
-        }, options);
+  $.fn.legSlideshow = function(options) {
+    var settings = $.extend(
+      {
+        slides: []
+      },
+      options
+    );
 
-        function legSlideshow(elem) {
-            var _this = {
-                _elem: elem,
-                currentIndex: 0
-            }
+    function legSlideshow(elem) {
+      var _this = {
+        _elem: elem,
+        currentIndex: 0
+      };
 
-            var isSliding = false;
+      var isSliding = false;
 
-            _this.selectSlide = function(index) {
-                if (isSliding) return;
-                
-                var promises = [];
-                isSliding = true;
-                if (index >= 0 && Array.isArray(settings.slides) && index < settings.slides.length) {
-                    var slide = settings.slides[index];
-                    $.each( slide, function( key, value ) {
-                        if (key === 'background') {
-                            $(_this._elem).find('[data-slideshow-background]').addBack('[data-slideshow-background]').css({'background-image': 'url(' + value + ')'});
-                        } else {
-                            var _placeholder = $(_this._elem).find("[data-slideshow-placeholder='" + key + "']");
-                            _placeholder.append('<span class="absolute-overlay">' + value + '</span>');
+      _this.selectSlide = function(index) {
+        if (isSliding) return;
 
-                            var delay = _placeholder.data('slideshowAnimDelay');
+        var promises = [];
+        isSliding = true;
+        if (
+          index >= 0 &&
+          Array.isArray(settings.slides) &&
+          index < settings.slides.length
+        ) {
+          var slide = settings.slides[index];
+          $.each(slide, function(key, value) {
+            if (key === "background") {
+              $(_this._elem)
+                .find("[data-slideshow-background]")
+                .addBack("[data-slideshow-background]")
+                .css({ "background-image": "url(" + value + ")" });
+            } else {
+              var _placeholder = $(_this._elem).find(
+                "[data-slideshow-placeholder='" + key + "']"
+              );
+              _placeholder.append(
+                '<span class="absolute-overlay">' + value + "</span>"
+              );
 
-                            let defer = $.Deferred();
-                            promises.push(defer);
+              var delay = _placeholder.data("slideshowAnimDelay");
 
-                            _placeholder.children().eq(0).delay(0).fadeTo(400, 0, 'swing');
-                            _placeholder.children().eq(1).delay(delay + 100).fadeTo(500, 1, 'swing', function() {
-                                _placeholder.children().eq(1).remove();
-                                _placeholder.children().eq(0).text(value);
-                                _placeholder.children().eq(0).css({opacity:1});
-                                defer.resolve();
-                            });
-                        }
-                    });
-                }
-                $.when.all(promises).then(function(){
-                    isSliding = false;
+              let defer = $.Deferred();
+              promises.push(defer);
+
+              _placeholder
+                .children()
+                .eq(0)
+                .delay(0)
+                .fadeTo(400, 0, "swing");
+              _placeholder
+                .children()
+                .eq(1)
+                .delay(delay + 100)
+                .fadeTo(500, 1, "swing", function() {
+                  _placeholder
+                    .children()
+                    .eq(1)
+                    .remove();
+                  _placeholder
+                    .children()
+                    .eq(0)
+                    .text(value);
+                  _placeholder
+                    .children()
+                    .eq(0)
+                    .css({ opacity: 1 });
+                  defer.resolve();
                 });
             }
-
-            _this.next = function() {
-                if (isSliding) return;
-
-                _this.currentIndex++;
-                if (_this.currentIndex > settings.slides.length - 1) {
-                    _this.currentIndex = 0;
-                }
-                _this.selectSlide(_this.currentIndex);
-            }
-
-            _this.prev = function() {
-                if (isSliding) return;
-                
-                _this.currentIndex--;
-                if (_this.currentIndex < 0) {
-                    _this.currentIndex = settings.slides.length - 1;
-                }
-                _this.selectSlide(_this.currentIndex);
-            }
-
-            //events
-            $(_this._elem).find('[data-slideshow-next]').click(_this.next);
-            $(_this._elem).find('[data-slideshow-prev]').click(_this.prev);
-
-            // load all images nao
-            $.each( settings.slides, function( key, value ) {
-                if (typeof value.background == 'string') {
-                    preloadImage(value.background);
-                }
-            });
-
-            //init
-            _this.selectSlide(0);
+          });
         }
-
-        return this.each( function() {
-            legSlideshow(this);
+        $.when.all(promises).then(function() {
+          isSliding = false;
         });
+      };
+
+      _this.next = function() {
+        if (isSliding) return;
+
+        _this.currentIndex++;
+        if (_this.currentIndex > settings.slides.length - 1) {
+          _this.currentIndex = 0;
+        }
+        _this.selectSlide(_this.currentIndex);
+      };
+
+      _this.prev = function() {
+        if (isSliding) return;
+
+        _this.currentIndex--;
+        if (_this.currentIndex < 0) {
+          _this.currentIndex = settings.slides.length - 1;
+        }
+        _this.selectSlide(_this.currentIndex);
+      };
+
+      //events
+      $(_this._elem)
+        .find("[data-slideshow-next]")
+        .click(_this.next);
+      $(_this._elem)
+        .find("[data-slideshow-prev]")
+        .click(_this.prev);
+
+      // load all images nao
+      $.each(settings.slides, function(key, value) {
+        if (typeof value.background == "string") {
+          preloadImage(value.background);
+        }
+      });
+
+      //init
+      _this.selectSlide(0);
     }
 
-}(jQuery));
+    return this.each(function() {
+      legSlideshow(this);
+    });
+  };
+})(jQuery);
 
-$('.legSlideshow').legSlideshow({
-    slides: [
-        { position: '01', background: '/assets/wir/coreValue/background1.jpg', title: 'Unsere Werte', description: 'Gute Arbeit ensteht aus Überzeugung.'},
-        { position: '02', background: '/assets/wir/coreValue/background2.jpg', title: 'Unsere Werte2', description: 'Gute Arbeit ensteht aus Überzeugung.2'},
-        { position: '03', background: '/assets/wir/coreValue/background3.jpg', title: 'Unsere Werte3', description: 'Gute Arbeit ensteht aus Überzeugung.3'}
-    ]
+$(".legSlideshow").legSlideshow({
+  slides: [
+    {
+      position: "01",
+      background: "/assets/wir/coreValue/background1.jpg",
+      title: "Unsere Werte",
+      description: "Gute Arbeit ensteht aus Überzeugung."
+    },
+    {
+      position: "02",
+      background: "/assets/wir/coreValue/background2.jpg",
+      title: "Unsere Werte",
+      description: "Ein Team ohne Individuen ist nicht kreativ."
+    },
+    {
+      position: "03",
+      background: "/assets/wir/coreValue/background3.jpg",
+      title: "Unsere Werte",
+      description: "Beginne bevor du bereit bist."
+    }
+  ]
 });
